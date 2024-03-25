2c033c4168f1a8622fc4123bc785dd00e16e915f
nathanchou21 <nathanchou21@mittymonarch.com>
2024-03-25 16:47:34 +0800

    hi

--- src/parser/golang/index.ts
+++ src/parser/golang/index.ts
@@ -41,15 +41,15 @@ parse(
     return 'FullGoParser'
   }
 }
-import { GoLexer } from './GoLexer.js';
-import { GoParser } from './GoParser.js';
-import { CharStream, CommonTokenStream } from 'antlr4';
-
-
-const input = "if (3 != 2) {val x := 3;} else {val y := 2;};"
-const chars = new CharStream(input); // replace this with a FileStream as required
-const lexer = new GoLexer(chars);
-const tokens = new CommonTokenStream(lexer);
-const parser = new GoParser(tokens);
-const tree = parser.expression();
-console.log(tree.toStringTree);
+// import { GoLexer } from './GoLexer.js';
+// import { GoParser } from './GoParser.js';
+// import { CharStream, CommonTokenStream } from 'antlr4';
+//
+//
+// const input = "if (3 != 2) {val x := 3;} else {val y := 2;};"
+// const chars = new CharStream(input); // replace this with a FileStream as required
+// const lexer = new GoLexer(chars);
+// const tokens = new CommonTokenStream(lexer);
+// const parser = new GoParser(tokens);
+// const tree = parser.expression();
+// console.log(tree.toStringTree);
